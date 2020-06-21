729a354b52213f87a0e4bf72160208e53dd4cc60
Bence Kovács <kovacs.ben.p@gmail.com>
2020-06-21 20:17:09 +0200

    fixed method name in tests, added implementation for enum types

--- src/main/java/com/codecool/VendingMachine/Inventory/BeverageType.java
+++ src/main/java/com/codecool/VendingMachine/Inventory/BeverageType.java
@@ -1,5 +1,20 @@
 package com.codecool.VendingMachine.Inventory;
 
 public enum BeverageType {
-
+	
+	Coke(25),
+	Pepsi(35),
+	Soda(45);
+	
+	private final int price;
+	
+	private BeverageType(int price) 
+	{
+		this.price = price;
+	}
+	
+	public int getPrice() 
+	{
+		return this.price;
+	}
 }
--- src/test/java/com/codecool/VendingMachine/Inventory/BeverageTypeTest.java
+++ src/test/java/com/codecool/VendingMachine/Inventory/BeverageTypeTest.java
@@ -10,19 +10,19 @@ public class BeverageTypeTest
 	@Test
 	public void cokePriceIs25() 
 	{
-		assertThat(BeverageType.Coke.getValue(), is(25));
+		assertThat(BeverageType.Coke.getPrice(), is(25));
 	}
 	
 	@Test
 	public void pepsiPriceIs35() 
 	{
-		assertThat(BeverageType.Pepsi.getValue(), is(35));
+		assertThat(BeverageType.Pepsi.getPrice(), is(35));
 	}
 	
 	@Test
 	public void sodaPriceIs45() 
 	{
-		assertThat(BeverageType.Soda.getValue(), is(45));
+		assertThat(BeverageType.Soda.getPrice(), is(45));
 	}
 	
 }
