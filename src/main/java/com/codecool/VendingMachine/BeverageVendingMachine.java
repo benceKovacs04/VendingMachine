4ae35b9fc5955d5f963bb53bc9b273b460436133
Bence Kovács <kovacs.ben.p@gmail.com>
2020-06-21 20:04:14 +0200

    added missing class, eclipse dont autosave so added default interface implementations

--- src/main/java/com/codecool/VendingMachine/BeverageVendingMachine.java
+++ src/main/java/com/codecool/VendingMachine/BeverageVendingMachine.java
@@ -1,5 +1,25 @@
 package com.codecool.VendingMachine;
 
-public class BeverageVendingMachine {
+import com.codecool.VendingMachine.Inventory.Beverage.Beverage;
+
+public class BeverageVendingMachine implements VendingMachine {
+
+	@Override
+	public void insertCoin(Coins coin) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int refund() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Beverage getBeverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/codecool/VendingMachine/Inventory/BeverageInventory.java
+++ src/main/java/com/codecool/VendingMachine/Inventory/BeverageInventory.java
@@ -1,5 +1,25 @@
 package com.codecool.VendingMachine.Inventory;
 
-public class BeverageInventory {
+import com.codecool.VendingMachine.Inventory.Beverage.Beverage;
+
+public class BeverageInventory implements Inventory {
+
+	@Override
+	public void resetInventory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Beverage getBeverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getInventoryInfo() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
